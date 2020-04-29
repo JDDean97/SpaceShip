d63394431db8700824bfdcc2f14e7131ddb19061
Jacob Dean <jcb_dean@yahoo.com>
2020-04-29 16:10:57 -0400

    Enemy script changes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,22 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class enemy : MonoBehaviour
 {
     GameObject player;
     Rigidbody rb;
     float speed;
+    public int health;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody>();
+        health = 100;
+    }
+    public void TakeDamage(int amount) {
+        health = health - amount;
+       
+        if (health <= 0) {
+            //player dies
+            Die();
+           
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Die() {
+        Destroy(gameObject);
     }
 
     private void FixedUpdate()
@@ -36,7 +45,6 @@ public class Enemy : MonoBehaviour
     void turn()
     {
         Quaternion rot = Quaternion.LookRotation(player.transform.position - transform.position, player.transform.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot,1 + 1*speed);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, 1 + 1 * speed);
     }
-
 }
